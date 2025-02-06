5cce9b5823db7345e924efb31a98bb2de1843bda
Alejandro Molia Tramullas <alexgrado39@gmail.com>
2025-02-06 15:45:12 +0100

    Empezado componente Examenes

--- frontend/mi-aplicacion-react/src/components/Examenes.js
+++ frontend/mi-aplicacion-react/src/components/Examenes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '../css/examenesStyle.css';
 
 const Examenes = () => {
 
@@ -9,8 +10,36 @@ const Examenes = () => {
     console.log(asignaturas)
 
     return (
-        <div>
-            <h1>Examenes</h1>
+        <div className="todoExamenes">
+            {asignaturas.map((asignatura, index) => (
+                <div>
+                    <h1>{asignatura.nombreAsignatura} <span className="asignaturaId">{asignatura.idAsignatura}</span></h1>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th colSpan="4">Febrero</th>
+                            <th colSpan="4">Junio</th>
+                            <th colSpan="4">Septiembre</th>
+                        </tr>
+                        <tr>
+                            <th>Curso</th>
+                            <th>1ª Sem.</th>
+                            <th>2ª Sem.</th>
+                            <th>Orig.</th>
+                            <th>Res.</th>
+                            <th>1ª Sem.</th>
+                            <th>2ª Sem.</th>
+                            <th>Orig.</th>
+                            <th>Res.</th>
+                            <th>1º Parc.</th>
+                            <th>2º Parc.</th>
+                            <th>Orig.</th>
+                            <th>Res.</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </div>
+            ))}
         </div>
     );
 };
